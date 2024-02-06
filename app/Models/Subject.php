c306855a221a40b87eef8c5c89c89c10d93be198
Funwi Kelsea <funwikelseandohnwi@gmail.com>
2024-02-06 20:54:22 +0100

    Learned how to perform morphic relationships

---
+++ app/Models/Subject.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    use HasFactory;
+
+    public function teachers() {
+        return $this->morphedByMany(Teacher::class, 'courseable');
+    }
+
+    public function students() {
+        return $this->morphedByMany(Student::class, 'courseable');
+    }
+}
---
+++ app/Models/Teacher.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphToMany;
+
+class Teacher extends Model
+{
+    use HasFactory;
+
+    public function subjects(): MorphToMany {
+        return $this->morphToMany(Subject::class, 'courseable');
+    }
+}
